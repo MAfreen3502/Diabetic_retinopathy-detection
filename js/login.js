8c946df6086b622f1afd750f24f2791523da4e54
Afreen <mullaafreen999@gmail.com>
2026-02-25 11:55:54 +0530

    Fix login redirect for GitHub Pages

--- js/login.js
+++ js/login.js
@@ -1,15 +1,6 @@
-document.getElementById("loginForm").addEventListener("submit", function (e) {
-
+document.getElementById("loginForm").addEventListener("submit", function(e){
     e.preventDefault();
 
-    fetch("http://127.0.0.1:5000/test")
-        .then(res => res.json())
-        .then(data => {
-            alert(data.msg);   // should show: backend is working
-        })
-        .catch(err => {
-            alert("Backend not reachable");
-            console.log(err);
-        });
-
+    // Redirect to image.html after login
+    window.location.href = "image.html";
 });
